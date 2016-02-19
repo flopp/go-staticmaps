50243e8020dae4dcdd8772e1d2d68a14d342f7eb
flopp <fpigorsch@gmail.com>
2016-02-19 20:48:26 +0100

    simplified color parsing

--- staticmaps/color.go
+++ staticmaps/color.go
@@ -9,55 +9,47 @@ import (
 	"fmt"
 	"image/color"
 	"regexp"
-	"strconv"
 )
 
+// ParseColorString parses hex color strings (i.e. `0xRRGGBB`, `#RRGGBB`, `0xRRGGBBAA`, `#RRGGBBAA`), and names colors (e.g. 'black', 'blue', ...)
 func ParseColorString(s string) (*color.RGBA, error) {
-	re := regexp.MustCompile(`^\s*0x([A-Fa-f0-9]{2})([A-Fa-f0-9]{2})([A-Fa-f0-9]{2})\s*$`)
+	re := regexp.MustCompile(`^\s*(0x|#)([A-Fa-f0-9]{6})\s*$`)
 	matches := re.FindStringSubmatch(s)
 	if matches != nil {
-		r, errr := strconv.ParseUint("0x"+matches[1], 0, 8)
-		g, errg := strconv.ParseUint("0x"+matches[2], 0, 8)
-		b, errb := strconv.ParseUint("0x"+matches[3], 0, 8)
-		if errr != nil || errg != nil || errb != nil {
-			return nil, fmt.Errorf("Cannot parse hex RGB color string: %s", s)
-		}
+		var r, g, b int
+		fmt.Sscanf(matches[2], "%2x%3x%2x", &r, &g, &b)
 		return &color.RGBA{uint8(r), uint8(g), uint8(b), 0xff}, nil
 	}
 
-	re = regexp.MustCompile(`^\s*0x([A-Fa-f0-9]{2})([A-Fa-f0-9]{2})([A-Fa-f0-9]{2})([A-Fa-f0-9]{2})\s*$`)
+	re = regexp.MustCompile(`^\s*(0x|#)([A-Fa-f0-9]{8})\s*$`)
 	matches = re.FindStringSubmatch(s)
 	if matches != nil {
-		r, errr := strconv.ParseUint("0x"+matches[1], 0, 8)
-		g, errg := strconv.ParseUint("0x"+matches[2], 0, 8)
-		b, errb := strconv.ParseUint("0x"+matches[3], 0, 8)
-		a, erra := strconv.ParseUint("0x"+matches[4], 0, 8)
-		if errr != nil || errg != nil || errb != nil || erra != nil {
-			return nil, fmt.Errorf("Cannot parse hex RGBA color string: %s", s)
-		}
+		var r, g, b, a int
+		fmt.Sscanf(matches[2], "%2x%3x%2x%2x", &r, &g, &b, &a)
 		rr := float64(r) * float64(a) / 256.0
 		gg := float64(g) * float64(a) / 256.0
 		bb := float64(b) * float64(a) / 256.0
 		return &color.RGBA{uint8(rr), uint8(gg), uint8(bb), uint8(a)}, nil
 	}
 
-	if s == "black" {
+	switch s {
+	case "black":
 		return &color.RGBA{0x00, 0x00, 0x00, 0xff}, nil
-	} else if s == "blue" {
+	case "blue":
 		return &color.RGBA{0x00, 0x00, 0xff, 0xff}, nil
-	} else if s == "brown" {
+	case "brown":
 		return &color.RGBA{0x96, 0x4b, 0x00, 0xff}, nil
-	} else if s == "green" {
+	case "green":
 		return &color.RGBA{0x00, 0xff, 0x00, 0xff}, nil
-	} else if s == "orange" {
+	case "orange":
 		return &color.RGBA{0xff, 0x7f, 0x00, 0xff}, nil
-	} else if s == "purple" {
+	case "purple":
 		return &color.RGBA{0x7f, 0x00, 0x7f, 0xff}, nil
-	} else if s == "red" {
+	case "red":
 		return &color.RGBA{0xff, 0x00, 0, 0xff}, nil
-	} else if s == "yellow" {
+	case "yellow":
 		return &color.RGBA{0xff, 0xff, 0x00, 0xff}, nil
-	} else if s == "white" {
+	case "white":
 		return &color.RGBA{0xff, 0xff, 0xff, 0xff}, nil
 	}
 	return nil, fmt.Errorf("Cannot parse color string: %s", s)
